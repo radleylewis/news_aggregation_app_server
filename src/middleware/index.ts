0c9192836bb7fed25f69c79a7f29ababf9f34c4e
Radley Sidwell-Lewis <rad.lewis@zoho.com>
2020-05-01 16:43:54 +0200

    feat: error catch middleware

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,5 +1,9 @@
 import authenticate from './authentication.middleware';
+import catchErrors from './error.middleware';
+import cors from './cors.middleware';
 
 export {
   authenticate,
+  catchErrors,
+  cors,
 };
