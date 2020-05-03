4b59cff8684081aced6be906ce8be84c0ff57d31
Radley Sidwell-Lewis <rad.lewis@zoho.com>
2020-05-03 23:45:08 +0200

    feat: fetch API sources

--- src/processes/index.ts
+++ src/processes/index.ts
@@ -1,10 +1,16 @@
 import { spawn } from 'child_process';
 
-const sourceUpdateProcess = spawn(process.execPath, [`${__dirname}/source.process.js`], {
-  detached: true,
-  stdio: [null, null, null, 'pipe'],
-});
-sourceUpdateProcess.stdio[3].on('data', (data) => {
-  // tslint:disable-next-line:no-console
-  console.log(data);
-});
+const getLatestArticles = () => {
+  const latestArticlesProcess = spawn(process.execPath, [`${__dirname}/article.process.js`], {
+    detached: true,
+    stdio: [null, null, null, 'pipe'],
+  });
+  latestArticlesProcess.stdio[3].on('data', (data) => {
+    // tslint:disable-next-line:no-console
+    console.log(data);
+  });
+};
+
+export {
+  getLatestArticles,
+};
